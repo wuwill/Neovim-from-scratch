0458f9819bbe5f6f9b62e9490a4024d9f608708e
Wei Yang <wyang@wym1.local.dhcp.wustl.edu>
2023-02-09 17:07:29 -0800

    ~nvim-tree - floating window

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -9,12 +9,57 @@ if not config_status_ok then
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+local HEIGHT_RATIO = 0.7 -- You can change this
+local WIDTH_RATIO = 0.4  -- You can change this too
 
-nvim_tree.setup {
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
+nvim_tree.setup({
+  disable_netrw = true,
+  hijack_netrw = true,
+  respect_buf_cwd = true,
+  sync_root_with_cwd = true,
+  view = {
+    relativenumber = true,
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        -- local center_y = ((vim.opt.lines:get() - window_h) / 2)
+        local center_y = (vim.opt.lines:get() / 2 - window_h)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = "rounded",
+          relative = "editor",
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
+    mappings = {
+      list = {
+        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
+        { key = "v", cb = tree_cb "vsplit" },
+      },
+    },
   },
+-- nvim_tree.setup {
+  -- update_focused_file = {
+  --   enable = true,
+  --   update_cwd = true,
+  -- },
   renderer = {
     root_folder_modifier = ":t",
     icons = {
@@ -53,15 +98,16 @@ nvim_tree.setup {
       error = "",
     },
   },
-  view = {
-    width = 30,
-    side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
-  },
-}
+  -- view = {
+  --   width = 30,
+  --   side = "left",
+  --   mappings = {
+  --     list = {
+  --       { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+  --       { key = "h", cb = tree_cb "close_node" },
+  --       { key = "v", cb = tree_cb "vsplit" },
+  --     },
+  --   },
+  -- },
+})
+
